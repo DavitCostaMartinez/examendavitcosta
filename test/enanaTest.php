dae415ca0ec026cf5d9224d7a678f5253efdbcc7
DavitCostaMartinez <92036332+DavitCostaMartinez@users.noreply.github.com>
2024-02-06 12:02:52 +0100

    FALLA

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -24,7 +24,10 @@ class EnanaTest extends TestCase {
     public function testHeridaLeveMuere() {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
-
+        $enana = new Enana("maria",9);
+        $enana->heridaLeve();
+        $this->assertEquals("muerta", $enana->getSituacion());
+        $this->assertLessThan(0, $enana->getPuntosVida());
     }
 
     public function testHeridaGrave() {
